f55ed05d338803933abb5e3cdf9bd1f80afd74e9
Maxence <maxgrymonprez@live.fr>
2022-02-22 15:20:22 +0000

    Add lastversion installation script

---
+++ scripts/02__install-lastversion.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# install lastversion tool.
+#
+# related documenations:
+# - lastversion Github page <https://github.com/dvershinin/lastversion>
+
+# install Python module
+pip3 install lastversion
